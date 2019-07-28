a17aa1b95f5e2cb0188925b5172a477966aaa499
wjbsyc <wjbsyc@hotmail.com>
2019-07-28 23:28:45 +0800

    ??

--- N1CTF/resources/views/create.blade.php
+++ N1CTF/resources/views/create.blade.php
@@ -5,7 +5,7 @@
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
-                <div class="panel-heading">{{$status or '新增'}}</div>
+                <div class="panel-heading">{{$status ?? '新增'}}</div>
                 <div class="panel-body">
                     <form action="{{ url('newchallenge') }}" method="POST">
                         {!! csrf_field() !!}
