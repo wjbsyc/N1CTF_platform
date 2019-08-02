6e269a441aad2474cc04f40208e06a1d2e24665b
wjbsyc <wjbsyc@hotmail.com>
2019-08-02 23:44:14 +0800

    sss

--- N1CTF/resources/views/auth/verify.blade.php
+++ N1CTF/resources/views/auth/verify.blade.php
@@ -4,10 +4,10 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Verify Your Email Address') }}</div>
-
-                <div class="card-body">
+            <br>
+             <div class="panel panel-default">
+                 <div class="panel-heading">Verify Your Email Address</div>
+                <div class="panel-body">
                     @if (session('resent'))
                         <div class="alert alert-success" role="alert">
                             {{ __('A fresh verification link has been sent to your email address.') }}
